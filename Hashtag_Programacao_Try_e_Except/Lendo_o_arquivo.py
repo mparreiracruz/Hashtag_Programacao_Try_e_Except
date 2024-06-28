32f2ac1659dafcfbd5c7388d6c8d92bf6438b73b
Matheus <147951308+mparreiracruz@users.noreply.github.com>
2024-06-28 15:31:02 -0300

    Update Lendo_o_arquivo.py

--- Hashtag_Programacao_Try_e_Except/Lendo_o_arquivo.py
+++ Hashtag_Programacao_Try_e_Except/Lendo_o_arquivo.py
@@ -2,7 +2,11 @@
 # b = 0
 #
 # print(a/b)
+try:
 
-with open('arquivo.txt', 'r') as file_object:
-    texto = file_object.read()
-    print(texto)
+    with open('arquivo.txt', 'r') as file_object:
+        texto = file_object.read()
+        print(texto)
+
+except:
+    print('Deu ruim')
